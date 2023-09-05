57e6338491bc7d1ef957a649a7fc7afc62121756
Junaid Ansari <junaidshahid.ansari@gmail.com>
2023-09-05 21:46:27 +0530

    Mutations and Error Handling

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { GraphQLError } = require('graphql')
 
+const {v1: uuid} = require('uuid')
 
 let persons = [
 	{
@@ -45,6 +47,15 @@ const typeDefs = `
 		findPerson(name: String!): Person
 	}
 
+	type Mutation {
+		addPerson(
+			name: String!
+			phone: String
+			street: String!
+			city: String!
+		): Person
+	}
+
 `
 
 const resolvers = {
@@ -63,7 +74,28 @@ const resolvers = {
 				city: root.city
 			}
 		}
-	}
+	},
+
+	Mutation: {
+		addPerson: (root, args) => {
+			
+			// error handling
+			if (persons.find(p => p.name === args.name)) {
+				throw new GraphQLError('Name must be unique', {
+					extensions: {
+						code: 'BAD_USER_INPUT',
+						invalidArgs: args.name
+					}
+				})
+			}
+
+			const person = {...args, id: uuid()}
+			persons = persons.concat(person)
+			return person
+
+		}
+	} 
+
 }
 
 const server = new ApolloServer({
