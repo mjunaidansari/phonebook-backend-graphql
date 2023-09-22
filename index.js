13ce0c35fb9776d3b39f9760fd79f9f181937ebc
Junaid Ansari <junaidshahid.ansari@gmail.com>
2023-09-22 21:28:05 +0530

    configuring backend gfor graphql subscriptions

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
-const { GraphQLError } = require('graphql')
+const { expressMiddleware } = require('@apollo/server/express4')
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
 
-const typeDefs = require('./GraphQL/typeDefs')
+const typeDefs = require('./GraphQL/schema')
 const resolvers = require('./GraphQL/resolvers')
 
 // for creating id
@@ -29,47 +34,62 @@ mongoose.connect(MONGODB_URI)
 		console.log('Error connection to MongoDB: ', error.message)
 	})
 
-// let persons = [
-// 	{
-// 		name: "Arto Hellas",
-// 		phone: "040-123543",
-// 		street: "Tapiolankatu 5 A",
-// 		city: "Espoo",
-// 		id: "3d594650-3436-11e9-bc57-8b80ba54c431"
-// 	},
-// 	{
-// 		name: "Matti Luukkainen",
-// 		phone: "040-432342",
-// 		street: "Malminkaari 10 A",
-// 		city: "Helsinki",
-// 		id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-// 	},
-// 	{
-// 		name: "Venla Ruuska",
-// 		street: "Nallemäentie 22 C",
-// 		city: "Helsinki",
-// 		id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-// 	},
-//   ]
+const start = async () => {
+
+	const app = express()
+	const httpServer = http.createServer(app)
+
+	const server = new ApolloServer({
+		schema: makeExecutableSchema({typeDefs, resolvers}),
+		plugins: [ApolloServerPluginDrainHttpServer({httpServer})],
+	})
+
+	await server.start()
+
+	app.use(
+		'/',
+		cors(),
+		express.json(),
+		expressMiddleware(server, {
+			context: async ({req}) => {
+				const auth = req ? req.headers.authorization : null
+				if (auth && auth.startsWith('Bearer ')) {
+					const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+					const currentUser = await User.findById(decodedToken.id).populate('friends')
+					return { currentuser }
+				}
+			}
+		})
+	)
+
+	const PORT = 4000
+
+	httpServer.listen(PORT, () => {
+		console.log(`Server is now running on http://localhost:${PORT}`)
+	})
+
+}
+
+start()
 
 
-const server = new ApolloServer({
-	typeDefs, // Contains the GraphQL Schema
-	resolvers, // describes how GraphQL queries are responded to
-})
+// const server = new ApolloServer({
+// 	typeDefs, // Contains the GraphQL Schema
+// 	resolvers, // describes how GraphQL queries are responded to
+// })
 
-startStandaloneServer(server, {
-	listen: {port: 4000},
-	context: async ({req, res}) => {
-		const auth = req ? req.headers.authorization : null
-		if (auth && auth.startsWith('Bearer ')) {
-			const decodedToken = jwt.verify(
-				auth.substring(7), process.env.JWT_SECRET
-			)
-			const currentUser = await User.findById(decodedToken.id).populate('friends')
-			return {currentUser} // the object returned is given as third parameter to all resolvers
-		}
-	}
-}).then(({url}) => {
-	console.log(`Server ready at ${url}`)
-})
+// startStandaloneServer(server, {
+// 	listen: {port: 4000},
+// 	context: async ({req, res}) => {
+// 		const auth = req ? req.headers.authorization : null
+// 		if (auth && auth.startsWith('Bearer ')) {
+// 			const decodedToken = jwt.verify(
+// 				auth.substring(7), process.env.JWT_SECRET
+// 			)
+// 			const currentUser = await User.findById(decodedToken.id).populate('friends')
+// 			return {currentUser} // the object returned is given as third parameter to all resolvers
+// 		}
+// 	}
+// }).then(({url}) => {
+// 	console.log(`Server ready at ${url}`)
+// })
